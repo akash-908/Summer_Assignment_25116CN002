780ab9624632347596c65291bffaf4cf55ce1d21
akash-908 <akashchaturvedi7905215090@gmail.com>
2026-06-12 19:20:38 +0530

    th

--- day10/q37.cpp
+++ day10/q37.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     int n = 5;
 
-    for(int i = n; i >= 1; i--) {
-        for(int j = 1; j <= i; j++) {
-            cout << j;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
         }
         cout << endl;
     }
