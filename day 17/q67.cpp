54423f23d067a18b66cc8f1cdbe89a6a128438ae
akash-908 <akashchaturvedi7905215090@gmail.com>
2026-06-17 08:08:50 +0530

    rrg

---
+++ day 17/q67.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,m;
+
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+
+    cin>>m;
+    int b[m];
+
+    for(int i=0;i<m;i++)
+        cin>>b[i];
+
+    cout<<"Intersection:\n";
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i]==b[j])
+            {
+                cout<<a[i]<<" ";
+                break;
+            }
+        }
+    }
+}
