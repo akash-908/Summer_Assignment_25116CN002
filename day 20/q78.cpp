8ece4ed0d8c97f328043421db60e7e01e592e86c
akash-908 <akashchaturvedi7905215090@gmail.com>
2026-06-20 19:35:06 +0530

    hh

---
+++ day 20/q78.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter order of matrix: ";
+    cin>>n;
+
+    int a[10][10];
+    bool flag=true;
+
+    cout<<"Enter matrix:\n";
+
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin>>a[i][j];
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(a[i][j]!=a[j][i])
+            {
+                flag=false;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        cout<<"Symmetric Matrix";
+    else
+        cout<<"Not a Symmetric Matrix";
+
+    return 0;
+}
